299b8e9f780e8864a9358ce43b1bc9b42c959171
m0ric <andreicristian6@protonmail.com>
2020-12-12 16:53:04 +0200

    Add JSON dump/load functions

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
 import hashlib
+import json
 
 def hash(plaintext):
     _salt = "pkekspkekspkekspkekspkekspkekspkekspkeks00000000000000000000000000"
     hr = hashlib.scrypt(plaintext.encode("utf-8"), salt = _salt.encode("utf-8"),
     n = 16384, r = 8, p = 1)
     return hr.hex()
+
+def dump_json(PATH, DICTIONARY):
+    try:
+        with open(PATH, 'w') as fp:
+            json.dump(DICTIONARY, indent = 4)
+    except Exception as e:
+        raise e
+
+def load_json(PATH):
+    try:
+        with open(PATH, 'r') as fp:
+            return json.load(PATH)
+    except Exception as e:
+        print(e)
+        return None
