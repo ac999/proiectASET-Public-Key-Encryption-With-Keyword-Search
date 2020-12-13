a65d8b7a9768ba49fd62296baf4931178b5f7d21
m0ric <andreicristian6@protonmail.com>
2020-12-13 15:27:48 +0200

    Add HaveIBeenPWNED API call

--- utils.py
+++ utils.py
@@ -1,11 +1,31 @@
 import hashlib
 import json
+import requests
+import logger
 
 def hash(plaintext):
     _salt = "pkekspkekspkekspkekspkekspkekspkekspkeks00000000000000000000000000"
-    hr = hashlib.scrypt(plaintext.encode("utf-8"), salt = _salt.encode("utf-8"),
+    hr = hashlib.scrypt(plaintext.encode("utf-8"), salt = _salt.encode('utf-8'),
     n = 16384, r = 8, p = 1)
     return hr.hex()
+
+def hibp_password(password):
+    try:
+        hash = hashlib.sha1(password.encode())
+        hash = hash.hexdigest().upper()
+
+        response = requests.get("https://api.pwnedpasswords.com/range/{}"\
+        .format(hash[:5]))
+
+        if (hash[5:] in list(map(lambda x: x.decode('utf-8').split(':')[0],
+        response.content.splitlines()))):
+            return (True, False)
+
+        return (True, True)
+
+    except Exception as e:
+        print(e)
+        return (False, True)
 
 def dump_json(PATH, DICTIONARY):
     try:
@@ -23,3 +43,6 @@ def load_json(PATH):
     except Exception as e:
         print(e)
         raise e
+
+def create_json(**kwargs):
+    return json.dumps(kwargs)
